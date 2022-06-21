6207431fe3fc9498143ecb74bec89ac734282870
DanieleDiPrimo <102224755+DanieleDiPrimo@users.noreply.github.com>
2022-06-21 20:18:10 +0200

    Update richieste.blade.php

--- resources/views/richieste.blade.php
+++ resources/views/richieste.blade.php
@@ -5,6 +5,13 @@
         <title>LE TUE RICHIESTE</title>
         <script src = "{{asset('js/richieste.js')}}"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel = "stylesheet" href = "{{asset('css/home.css')}}">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+        <script src = "{{asset('js/navbar.js')}}" defer = "true"></script>
     </head>
 
     <body>
