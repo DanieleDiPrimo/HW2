8775180ac4aeaf127a9447c0171af742e3ea10e6
DanieleDiPrimo <102224755+DanieleDiPrimo@users.noreply.github.com>
2022-06-21 19:29:38 +0200

    Update cerca.blade.php

--- resources/views/cerca.blade.php
+++ resources/views/cerca.blade.php
@@ -4,6 +4,7 @@
     <head>
         <title>CERCA UTENTE</title>
         <link rel ="stylesheet" href = "{{asset('css/home.css')}}"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
